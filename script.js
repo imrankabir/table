f704b23415b1d4505d3b0a463fe5ad9f6de87e61
Imran Kabir <imran.kabir.k@gmail.com>
2024-11-25 12:41:07 +0500

    table auto commit on 2024-11-25 12:41:07

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-function generateTable() {
-  const tableNumber = document.querySelector('#tableNumber').value;
-  const tableContainer = document.querySelector('#tableContainer');
-  if (!tableNumber || tableNumber < 2 || tableNumber > 20) {
+const getNumber = e => localStorage.getItem('table-number') ?? 2;
+const saveNumber = number => localStorage.setItem('table-number', number);
+
+document.querySelector('#t').addEventListener('keyup', e => {
+    saveNumber(document.querySelector('#t').value);
+});
+
+const gt = e => {
+  const t = document.querySelector('#t').value;
+  const tc = document.querySelector('#tc');
+  if (!t || t < 2 || t > 20) {
     alert('Please enter a number between 2 and 20.');
     return;
   }
-  let tableHTML = `<table>`;
+  let th = '';
   for (let i = 1; i <= 10; i++) {
-    tableHTML += `<tr><td>${tableNumber} x ${i} = ${tableNumber * i}</td></tr>`;
+    th += `<tr><td>${t} x ${i} = ${t * i}</td></tr>`;
   }
-  tableHTML += `</table>`;
-  tableContainer.innerHTML = tableHTML;
-}
+  tc.innerHTML = th;
+};
 
-generateTable();
+(e => {
+    document.querySelector('#t').value = getNumber();
+    gt();
+})();
