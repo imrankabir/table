ce62089ec139f9fc8d68cdefa775fa0384bec183
Imran Kabir <imran.kabir.k@gmail.com>
2024-11-25 10:26:58 +0500

    table auto commit on 2024-11-25 10:26:58 AM

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 function generateTable() {
-  
   const tableNumber = document.querySelector('#tableNumber').value;
   const tableContainer = document.querySelector('#tableContainer');
-
   if (!tableNumber || tableNumber < 2 || tableNumber > 20) {
     alert('Please enter a number between 2 and 20.');
     return;
   }
-
   let tableHTML = `<table>`;
-
   for (let i = 1; i <= 10; i++) {
-    tableHTML += `
-      <tr>
-        <td>${tableNumber} x ${i} = ${tableNumber * i}</td>
-      </tr>
-    `;
+    tableHTML += `<tr><td>${tableNumber} x ${i} = ${tableNumber * i}</td></tr>`;
   }
-
   tableHTML += `</table>`;
   tableContainer.innerHTML = tableHTML;
 }
